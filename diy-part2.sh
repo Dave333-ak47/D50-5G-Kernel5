cc4d4698ebbf9f90e6d0c2bd129b0a447c89bfb7
Dave333-ak47 <davidecavecchia2@gmail.com>
2026-05-23 18:44:31 +0200

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# Rimuove eventuali pacchetti duplicati
 rm -rf package/luci-app-exigod50
+
+# Clona la tua app per la gestione del modem 5G
 git clone https://github.com/Dave333-ak47/d50-modem-feed package/luci-app-exigod50
+
+# Crea la cartella dei firmware nel sistema OpenWrt
 mkdir -p package/base-files/files/lib/firmware/
-if [ -d "wi-fi" ]; then
-    cp -r wi-fi/Bdwlan* package/base-files/files/lib/firmware/
-else
-    cp -r Bdwlan* package/base-files/files/lib/firmware/ 2>/dev/null
-fi
+
+# Copia i dati radio di calibrazione Bdwlan (cerca file e cartelle nella root)
+echo "Iniezione dati di calibrazione Wi-Fi in corso..."
+cp -r Bdwlan* package/base-files/files/lib/firmware/ 2>/dev/null
+
+echo "Script diy-part2.sh completato!"
