686eb9d7fcd8a75dd542239c822ed6c7581a23a7
Dave333-ak47 <davidecavecchia2@gmail.com>
2026-05-24 14:18:12 +0200

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-#echo 'src-git ekopackages https://github.com/ekozera/openwrt-packages.git' >> feeds.conf.default Script parte 1
+echo 'src-git ekopackages https://github.com/ekozera/openwrt-packages.git' >> feeds.conf.default Script parte 1
+echo 'src-git ekopackages https://github.com/ekozera/openwrt-packages.git' >> feeds.conf.default
